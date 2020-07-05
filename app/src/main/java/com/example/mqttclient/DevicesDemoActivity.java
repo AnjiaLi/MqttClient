d42768ed96cba1d0faa9118f22bb3d76cdf26d3f
Anjiali <1185938704@qq.com>
2020-07-05 15:49:27 +0800

    version-1.0.0

---
+++ app/src/main/java/com/example/mqttclient/DevicesDemoActivity.java
@@ -0,0 +1,383 @@
+package com.example.mqttclient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import com.example.mqttclient.mqtt.MqttService;
+import com.example.mqttclient.protocol.AirConditioningMessage;
+import com.example.mqttclient.protocol.BoolMessage;
+import com.example.mqttclient.protocol.FloatMessage;
+import com.example.mqttclient.protocol.IntMessage;
+import com.google.gson.Gson;
+
+import org.eclipse.paho.client.mqttv3.MqttException;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class DevicesDemoActivity extends AppCompatActivity implements MqttService.MqttEventCallBack, CompoundButton.OnCheckedChangeListener {
+
+    private TextView connectState, temperatureValue, humidityValue, pmValue, carbonDioxideValue,
+            gasValue, waterTowerValue, illuminanceValue , doorStatus, peopleStatus, windowStatus;
+    private EditText airCconditioningValue;
+    private MqttService.MqttBinder mqttBinder;
+    private String TAG = "MainActivity";
+    private Switch parlourLightSwitch, curtain_switch, fan_socket_switch, air_conditioning_switch;
+    private ImageView parlour_light_switch_image,door_status_image,people_status_image,fan_socket_switch_image,
+            window_status_image;
+    private Map<String, Integer> subscribeTopics = new HashMap<>();
+
+    private Button arrow_down,arrow_up;
+
+    private final Timer timer = new Timer();
+    int count=0;
+    int temperature=25;
+
+    TimerTask timerTask = new TimerTask() {
+        @Override
+        public void run() {
+            count++;
+            fan_socket_switch_image.animate().rotation(count%18*20);
+        }
+    };
+
+    Handler handler=new Handler();
+
+
+    Runnable runnable=new Runnable() {
+        @Override
+        public void run() {
+            count++;
+            fan_socket_switch_image.animate().rotation(count*20);
+            handler.postDelayed(this, 100);
+        }
+    };
+
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            mqttBinder = (MqttService.MqttBinder) iBinder;
+            mqttBinder.setMqttEventCallback(DevicesDemoActivity.this);
+            if (mqttBinder.isConnected()) {
+                connectState.setText("已连接");
+                subscribeTopics();
+            } else {
+                connectState.setText("未连接");
+            }
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_devices_demo);
+
+        connectState = findViewById(R.id.dev_connect_state);
+
+        Intent mqttServiceIntent = new Intent(this, MqttService.class);
+        bindService(mqttServiceIntent, connection, Context.BIND_AUTO_CREATE);
+
+        temperatureValue = findViewById(R.id.temperature_value);
+
+        humidityValue = findViewById(R.id.humidity_value);
+        pmValue = findViewById(R.id.pm_value);
+        gasValue = findViewById(R.id.gas_value);
+        carbonDioxideValue=findViewById(R.id.carbon_dioxide_value);
+        waterTowerValue=findViewById(R.id.waterTower_value);
+        illuminanceValue=findViewById(R.id.illuminance_value);
+        doorStatus = findViewById(R.id.door_status);
+        windowStatus=findViewById(R.id.window_status);
+        peopleStatus=findViewById(R.id.people_status);
+
+
+        airCconditioningValue = findViewById(R.id.air_conditioning_value);
+        parlourLightSwitch = findViewById(R.id.parlour_light_switch);
+        parlourLightSwitch.setOnCheckedChangeListener(this);
+        curtain_switch = findViewById(R.id.curtain_switch);
+        curtain_switch.setOnCheckedChangeListener(this);
+        fan_socket_switch = findViewById(R.id.fan_socket_switch);
+        fan_socket_switch.setOnCheckedChangeListener(this);
+        air_conditioning_switch = findViewById(R.id.air_conditioning_switch);
+        air_conditioning_switch.setOnCheckedChangeListener(this);
+
+
+        parlour_light_switch_image=findViewById(R.id.parlour_light_switch_image);
+        door_status_image=findViewById(R.id.door_status_image);
+        people_status_image=findViewById(R.id.people_status_image);
+        fan_socket_switch_image=findViewById(R.id.fan_socket_switch_image);
+        window_status_image=findViewById(R.id.window_status_image);
+        arrow_down=findViewById(R.id.arrow_down);
+        arrow_up=findViewById(R.id.arrow_up);
+
+        temperature = Integer.parseInt(airCconditioningValue.getText().toString());
+
+
+        arrow_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                temperature++;
+                airCconditioningValue.setText(String.valueOf(temperature));
+                if (air_conditioning_switch.isChecked()){
+                    try {
+                        String json = new Gson().toJson(new AirConditioningMessage(true,
+                                Float.parseFloat(airCconditioningValue.getText().toString())));
+                        Log.d("json",json);
+                        mqttBinder.publishMessage("/test/airConditioning",json);
+                    } catch (MqttException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        arrow_down.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                temperature--;
+                airCconditioningValue.setText(String.valueOf(temperature));
+                if (air_conditioning_switch.isChecked()){
+                    try {
+                        String json = new Gson().toJson(new AirConditioningMessage(true,
+                                Float.parseFloat(airCconditioningValue.getText().toString())));
+                        Log.d("json",json);
+                        mqttBinder.publishMessage("/test/airConditioning",json);
+                    } catch (MqttException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+        switch (compoundButton.getId()) {
+            case R.id.parlour_light_switch:
+                try {
+                    if (compoundButton.isChecked()) {
+                        mqttBinder.publishMessage("/test/light1",
+                                new Gson().toJson(new BoolMessage(true)));
+                        parlour_light_switch_image.setImageResource(R.drawable.light);
+
+                    } else {
+                        mqttBinder.publishMessage("/test/light1",
+                                new Gson().toJson(new BoolMessage(false)));
+                        parlour_light_switch_image.setImageResource(R.drawable.dark);
+                    }
+                } catch (MqttException e) {
+                    e.printStackTrace();
+                }
+                break;
+
+            case R.id.curtain_switch:
+                try {
+                    if (compoundButton.isChecked()) {
+                        mqttBinder.publishMessage("/test/curtain1",
+                                new Gson().toJson(new BoolMessage(true)));
+                    } else {
+                        mqttBinder.publishMessage("/test/curtain1",
+                                new Gson().toJson(new BoolMessage(false)));
+                    }
+                } catch (MqttException e) {
+                    e.printStackTrace();
+                }
+                break;
+
+            case R.id.fan_socket_switch:
+                try {
+                    if (compoundButton.isChecked()) {
+                        mqttBinder.publishMessage("/test/fan1",
+                                new Gson().toJson(new BoolMessage(true)));
+
+                        handler.postDelayed(runnable, 100);//每两秒执行一次runnable.
+
+
+                    } else {
+                        mqttBinder.publishMessage("/test/fan1",
+                                new Gson().toJson(new BoolMessage(false)));
+
+                        handler.removeCallbacks(runnable);
+                    }
+                } catch (MqttException e) {
+                    e.printStackTrace();
+                }
+                break;
+
+            case R.id.air_conditioning_switch:
+                try {
+                    if (compoundButton.isChecked()) {
+                        String json = new Gson().toJson(new AirConditioningMessage(true,
+                                Float.parseFloat(airCconditioningValue.getText().toString())));
+                        Log.d("json",json);
+                        mqttBinder.publishMessage("/test/airConditioning",json);
+                    } else {
+                        String json = new Gson().toJson(new AirConditioningMessage(false,
+                                Float.parseFloat(airCconditioningValue.getText().toString())));
+                        Log.d("json",json);
+                        mqttBinder.publishMessage("/test/airConditioning",json);
+                    }
+                } catch (MqttException e) {
+                    e.printStackTrace();
+                }
+                break;
+        }
+    }
+
+    void subscribeTopics() {
+        try {
+            subscribeTopics.put("/test/temp",1);
+            subscribeTopics.put("/test/hum", 2);
+            subscribeTopics.put("/test/pm",3);
+            subscribeTopics.put("/test/gas",4);
+            subscribeTopics.put("/test/door",5);
+            subscribeTopics.put("/test/co2",6);
+            subscribeTopics.put("/test/waterTower",7);
+            subscribeTopics.put("/test/illuminance",8);
+            subscribeTopics.put("/test/human",9);
+            subscribeTopics.put("/test/window",10);
+
+            for(Map.Entry<String, Integer> entry : subscribeTopics.entrySet()){
+                mqttBinder.subscribe(entry.getKey());
+            }
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void unSubscribeTopics() {
+        try {
+            for(Map.Entry<String, Integer> entry : subscribeTopics.entrySet()){
+                mqttBinder.unSubscribe(entry.getKey());
+            }
+            subscribeTopics.clear();
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onConnectSuccess() {
+        subscribeTopics();
+        connectState.setText("已连接");
+    }
+
+    @Override
+    public void onConnectError(String error) {
+        Log.d(TAG, "onConnectError: " + error);
+        connectState.setText("未连接");
+        subscribeTopics.clear();
+    }
+
+    @Override
+    public void onDeliveryComplete() {
+        Log.d(TAG, "publish ok");
+    }
+
+    @Override
+    public void onMqttMessage(String topic, String message) {
+        Log.d("onMqttMessage", "topic:"+topic+ "message length:"+ message.length() + ", message:"+message);
+        Gson gson = new Gson();
+        switch (subscribeTopics.get(topic)){
+            case 1:
+                temperatureValue.setText(String.valueOf(gson.fromJson(message.trim(), FloatMessage.class).value));
+                break;
+
+            case 2:
+                humidityValue.setText(String.valueOf(gson.fromJson(message.trim(), IntMessage.class).value));
+                break;
+
+            case 3:
+                pmValue.setText(String.valueOf(gson.fromJson(message.trim(), IntMessage.class).value));
+                break;
+
+            case 4:
+                gasValue.setText(String.valueOf(gson.fromJson(message.trim(), IntMessage.class).value));
+                break;
+
+            case 5:
+                String status = gson.fromJson(message.trim(), BoolMessage.class).value ?"开":"关";
+                doorStatus.setText(status);
+                if (status.equals("开")){
+                    door_status_image.setImageResource(R.drawable.open);
+                }else {
+                    door_status_image.setImageResource(R.drawable.close);
+                }
+                break;
+
+            case 6:
+                carbonDioxideValue.setText(String.valueOf(gson.fromJson(message.trim(), IntMessage.class).value));
+                break;
+
+            case 7:
+                waterTowerValue.setText(String.valueOf(gson.fromJson(message.trim(), IntMessage.class).value));
+                break;
+            case 8:
+                illuminanceValue.setText(String.valueOf(gson.fromJson(message.trim(), IntMessage.class).value));
+                break;
+
+            case 9:
+                String people_Status = gson.fromJson(message.trim(), BoolMessage.class).value ?"有":"无";
+                peopleStatus.setText(people_Status);
+                if (people_Status.equals("有")){
+                    people_status_image.setImageResource(R.drawable.people);
+                }else {
+                    people_status_image.setImageResource(R.drawable.nopeople);
+                }
+                break;
+            case 10:
+                String window_Status = gson.fromJson(message.trim(), BoolMessage.class).value ?"开":"关";
+                windowStatus.setText(window_Status);
+                if (window_Status.equals("开")){
+                    window_status_image.setImageResource(R.drawable.open_window);
+                }else {
+                    window_status_image.setImageResource(R.drawable.close_window);
+                }
+                break;
+        }
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        if (mqttBinder.isConnected()) {
+            connectState.setText("已连接");
+            subscribeTopics();
+        } else {
+            connectState.setText("未连接");
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unSubscribeTopics();
+    }
+
+    @Override
+    protected void onDestroy() {
+        unbindService(connection);
+        super.onDestroy();
+    }
+
+}
